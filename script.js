298970d3a909109827aaf1d48308695e9ac5ff38
Zenon <abhishekyadav276208@gmail.com>
2025-10-07 21:50:50 +0530

    minor changes

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const addTaskBtn = document.getElementById('add-task-button');
+
+addTaskBtn.addEventListener('click', () => {
+    const input = prompt("Enter the tasks to be done")
+    if (!input) return;
+    
+})
